6caa24adae7f6749150f1313cb7eb561dcb8a235
Tdmaxer <ytdilshan07@gmail.com>
2025-03-21 19:44:13 +0530

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,57 +1,34 @@
-import React from 'react'
-import logo from '../logo.png'
-
-//font awsome import
-
-
+import React from 'react';
+import logo from '../logo1.png';
 
 const NavBar = () => {
   return (
-    <nav id="scroll" className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className='container'>
-        <div className='container'>
-        
-        
-  <a class="navbar-brand" href="#">
-  <img className="logo" src={logo}  />
-</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    
-  </button>
-  </div>
-  
- 
+    <nav id="scroll" className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className='container'>
+        <a className="navbar-brand"  href="#">
+          <img className="logo" src={logo} alt="Logo" style={{ width: '80px', height: 'auto', position: 'fixed' }} />
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        </button>
+      </div>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto justify-content-end">
+          <li className="nav-item active">
+            <a className="nav-link" href="/" style={{ color: 'white' }}>Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#resume" style={{ color: 'white' }}>About me</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#projects" style={{ color: 'white' }}>Projects</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#contact" style={{ color: 'white' }}>Contact us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul className="navbar-nav ml-auto justify-content-end">
-
-  
-
-      <li className="nav-item active">
-        <a className="nav-link" href="/">Home </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#resume">About me</a>
-      </li>
-      {/* <li className="nav-item">
-        <a className="nav-link" href="#">Service</a>
-      </li> */}
-      <li className="nav-item">
-        <a className="nav-link" href="#projects">Projects</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#contact">contact us</a>
-      </li>
-
-
-
-        
-    </ul>
-    
-  </div>
-  </div>
-</nav>
-  )
-}
-
-export default NavBar
+export default NavBar;
