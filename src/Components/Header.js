f20441183d29ea1e910ba23bb1fc34f7c743b8bc
Tdmaxer <ytdilshan07@gmail.com>
2025-03-21 19:44:13 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,44 +1,61 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import logo from '../logo1.png';
+import '../CSS/Header.css';
 
 function Header() {
+  useEffect(() => {
+    const title = document.querySelector('.title-3d');
+
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      const centerX = window.innerWidth / 2;
+      const centerY = window.innerHeight / 2;
+
+      const rotateX = (clientY - centerY) * 0.01;
+      const rotateY = (clientX - centerX) * 0.01;
+
+      title.style.transform = `perspective(1000px) rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <div className="project-container-top-banner">
       <div className="container">
         <div className="row">
           <div className="col-md-12">
             <div className="main-info">
-              <h1 className="animate-charcter">I'M THARINDU DILSHAN</h1><br />
-              <h3>I PROVIDING</h3>
-              {/* <Typed
-                className="typed-text"
-                strings={[
-                  "Mern Web Design",
-                  "Android App Development",
-                  "UX/UI Design",
-                  "Creative Designing",
-                  "Graphic Designing",
-                ]}
-                typeSpeed={40}
-                backSpeed={60}
-                loop
-              /> */}
+              <h1 className="title-3d">I'M THARINDU DILSHAN</h1><br />
+              <h3 className='welcome-text' style={{
+                color: '#ffffff',
+                textShadow: '0 0 3px #fff, 0 0 5px #fff, 0 0 7px #49ff18, 0 0 10px #49ff18, 0 0 15px #49ff18'
+              }}>
+                Welcome to NextGen Labs
+              </h3>
+
+
+
               <br />
-              <h3 className="animate-charcter">Services.</h3>
+              <img className="logo" src={logo} alt="Logo" style={{ width: '100px', height: 'auto' }} />
+
+
+              <br></br>
               <div className="container">
                 <div className="row">
+
                   <div className="col-md-12">
-                    <button  type="button" className="btn-primary"><a id="resumebtn" href='#resume'>RESUME</a></button>
+                    <button type="button" className="btn-primary"><a id="resumebtn" href='#resume'>Getting Start</a></button>
                   </div>
                 </div>
               </div>
             </div>
             <br />
-            <i
-              data-mdb-toggle="animation"
-              data-mdb-animation-reset="true"
-              data-mdb-animation="slide-right"
-            ></i>
+
           </div>
         </div>
       </div>
