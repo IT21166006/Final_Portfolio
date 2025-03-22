721bf30df55e3425eec9d5d69f999e28735b2044
Tdmaxer <ytdilshan07@gmail.com>
2025-03-22 18:34:38 +0530

    Update Animater.js

--- src/Components/Animater.js
+++ src/Components/Animater.js
@@ -3,6 +3,7 @@ import Spline from '@splinetool/react-spline';
 export default function App() {
   return (
     <Spline scene="https://prod.spline.design/gtDMIUPR-n6qCPwu/scene.splinecode" />
+    
   );
 }
 
