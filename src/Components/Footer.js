cd5c6c791d2233d9646a3f59709a0933bc4571d5
Tdmaxer <ytdilshan07@gmail.com>
2025-03-22 18:34:39 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,56 +1,70 @@
-import React from 'react'
+import React from 'react';
+import logo from '../logo.png';
 
 const Footer = () => {
   return (
-    <div id='contact'>
-        <div className='footer'>
-        <div className='container'>
-            
-            <div className='row'>
-                
-                <div className='col-md-2 '></div>
-                <div className='col-md-8 '>
-                <br></br>
-               Get In Touch<br></br>
-               <a href="mailto:it21166006@my.sliit.lk" data-hover="Let's talk!" class="needdev">Need a developer?</a>
+    <footer className="bg-light text-muted">
+      <div className="container-fluid py-4">
+        <div className="row">
+          {/* Logo and Description */}
+          <div className="col-md-3 mb-4 px-5">
+            <h5 className="text-uppercase fw-bold">Nextgen Labs</h5>
+            <a className="navbar-brand"  href="#">
+          <img className="logo" src={logo} alt="Logo" style={{ width: '80px', height: 'auto', position: 'fixed' }} />
+        </a>
+            <p>
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.
+            </p>
+            {/* Social Media Icons */}
+            <div>
+              <a href="#!" className="me-3 text-reset"><i className="fab fa-facebook-f"></i></a>
+              <a href="#!" className="me-3 text-reset"><i className="fab fa-twitter"></i></a>
+              <a href="#!" className="me-3 text-reset"><i className="fab fa-instagram"></i></a>
+              <a href="#!" className="me-3 text-reset"><i className="fab fa-linkedin"></i></a>
             </div>
-            </div>
+          </div>
+
+          {/* Company Links */}
+          <div className="col-md-2 mb-4">
+            <h6 className="text-uppercase fw-bold">Company</h6>
+            <ul className="list-unstyled">
+              <li><a href="#!" className="text-reset">About</a></li>
+              <li><a href="#!" className="text-reset">Features</a></li>
+              <li><a href="#!" className="text-reset">Works</a></li>
+              <li><a href="#!" className="text-reset">Career</a></li>
+            </ul>
+          </div>
+
+          {/* Help Links */}
+          <div className="col-md-2 mb-4">
+            <h6 className="text-uppercase fw-bold">Help</h6>
+            <ul className="list-unstyled">
+              <li><a href="#!" className="text-reset">Customer Support</a></li>
+              <li><a href="#!" className="text-reset">Delivery Details</a></li>
+              <li><a href="#!" className="text-reset">Terms & Conditions</a></li>
+              <li><a href="#!" className="text-reset">Privacy Policy</a></li>
+            </ul>
+          </div>
+
+          {/* Newsletter Subscription */}
+          <div className="col-md-3 mb-4">
+            <h6 className="text-uppercase fw-bold">Subscribe to Newsletter</h6>
+            <form action="#">
+              <div className="input-group mb-3">
+                <input type="email" className="form-control" placeholder="Enter your email" />
+                <button type="submit" className="btn btn-primary">Subscribe</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <br></br>
-        <hr className="custom-hr" />
-        <div className='details'>
-        <div className='row'>
-                <div className='col-md-4'>
-                    Call Me
-                </div>
-                <div className='col-md-4'>
-                    Social
-                </div>
-                <div className='col-md-4'>
-                    Grab A Cofee
-                </div>
-                </div>
 
-                <div className='row'>
-                <div className='col-md-4'>
-                    +94 714753953
-                </div>
-                <div className='col-md-4'>
-                    <a className="linkedin" href='https://www.linkedin.com/in/tharindu-dilshan-543886285/'>Linkedin</a>
-                </div>
-                <div className='col-md-4'>
-                    it21166006@my.sliit.lk
-                </div>
-                </div></div>
-                <hr className="custom-hr" />
-                <footer>
-               <p className='copyright'>&copy; Copyright by TDMaxer</p>
-               </footer>
-
+        {/* Copyright Section */}
+        <div className="text-center py-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }}>
+          © Copyright 2021, All Rights Reserved by Postcrafts
         </div>
-        
-    </div>
-  )
-}
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
