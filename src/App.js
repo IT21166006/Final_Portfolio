63aefa2af6e2d3ad69e9f07bd23b703138cb2531
Tdmaxer <ytdilshan07@gmail.com>
2025-03-22 14:26:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
@@ -12,28 +12,45 @@ import Footer from './Components/Footer';
 import ScrollToTopButton from './Components/ScrollToTopButton';
 import Tech from './Components/tech';
 import Technology from './Components/Technologies';
-
-
-
-
-
+import Animater from './Components/Animater';
+import LoadingAnimation from './Components/LoadingAnimation';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 6000); // 5 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <LoadingAnimation />;
+  }
+
   return (
     <>
     
       <NavBar />
-      <Header />
+      <Animater/>
       
+      <br></br>
+      <Header />
+      <br></br>
  
 
-      <Projects/>
-      <br></br>
       <Resume/>
+      <br></br>
+      
+      <Projects/>
       <br></br>
       <Quote/>
       <br></br>
       <Technology/>
+      <br></br>
+     
       <br></br>
       <Footer/>
       <ScrollToTopButton />
