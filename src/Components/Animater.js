87fb14e0da3fe9025bbd10b59acde92fb405d984
Tdmaxer <ytdilshan07@gmail.com>
2025-03-22 14:26:26 +0530

    Add Animater.js

---
+++ src/Components/Animater.js
@@ -0,0 +1,12 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/gtDMIUPR-n6qCPwu/scene.splinecode" />
+  );
+}
+
+
+{/* <script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.79/build/spline-viewer.js"></script>
+<spline-viewer url="undefined"></spline-viewer> */}
+
